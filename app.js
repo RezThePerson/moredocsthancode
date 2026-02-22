a91ef8a8d9eb4c1dcc2f17b2ac974778cf770c7a
RezThePerson <rhythmupa@gmail.com>
2026-02-22 11:37:20 +0530

    right i need  to  use git. ts sop scuffed

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-export let target = "RezThePerson";
+export let target = "BaguetteSlayer";
 
 // config abovecode below
 
 import mc from "minecraft-protocol";
 
 import { LogPlayerPos } from "./lib/LogPlayerPos.js";
+import { LogPlayerHealth, getHealth } from "./lib/LogPlayerHealth.js";
 import { moveTowardsTarget } from "./lib/moveTowordsTarget.js";
+import { anchor } from "./lib/anchor.js";
 
 export const bot = mc.createClient({
-    host: "localhost",
+    host: "server.botfights.hackcraft.hackclub.com",
     username: "ineedhelp",
     version: "1.21.11",
     auth: "offline",
@@ -25,17 +27,27 @@ bot.on("position", (packet) => {
 });
 
 bot.on("login", (packet) => {
+    bot.write("chat_command", {
+        command: "login ineedhelp",
+    });
+
+    LogPlayerHealth();
     LogPlayerPos();
 
     setTimeout(() => {
-        setInterval(tick, 50);
+        setInterval(tick, 500);
     }, 500);
 });
 
 function tick() {
+    if (getHealth().health < 1) {
+        console.log("Health low, skipping tick");
+        return;
+    }
+
     let inrange = moveTowardsTarget() < 3 ? true : false;
 
     if (inrange) {
-        bot.chat("I'm close to the target!");
+        anchor(target);
     }
 }
